fd8a5e46b80a7a2ebff01a60fe12b6ae3a48ce18
vovalive <looongdog@gmail.com>
2017-09-08 15:43:16 +0300

    added tabs, help page

--- app.R
+++ app.R
@@ -18,7 +18,15 @@ org=as.matrix(org)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
-  
+  tags$head(
+    tags$style(
+      HTML(
+        "h3 {color:purple;}
+         img {box-shadow: 0 0 1em grey;}
+        #dark {color:navy;}"
+      )
+    )
+  ),
   # Application title
   titlePanel("VolcanoR"),
   # Sidebar with a slider input for number of bins 
@@ -26,7 +34,7 @@ ui <- shinyUI(fluidPage(
     sidebarPanel(
       p('Start with loading your DiffExpression results. We accept tab-separated files with columns: GeneSymbol Pvalue Log2FoldChange
         You can first download the sample',
-        a(href = 'https://yadi.sk/i/r9QNoKD03DMVec', 'res.txt'),
+        a(href = 'https://raw.githubusercontent.com/vovalive/volcanoR/master/res2_GSE70213.txt', 'res.txt'),
         'file for mouse muscular tissue differential expresion results, and then try uploading it.'),
       selectInput("org", "Organism:", 
                   choices=row.names(org)),
@@ -41,7 +49,7 @@ ui <- shinyUI(fluidPage(
                 )
       ),
       p('when your file is uploaded, you can click button below to get Volcano plot'),
-      actionButton("plot", "Generate Volcano plot"),
+      actionButton("plot", "Generate volcano plot"),
       p('you can adjust tresholds, it will change the look of your volcano plot and enrichment results'),
       sliderInput("fc.tr",
                   "log2 Fold change treshold:",
@@ -61,16 +69,30 @@ ui <- shinyUI(fluidPage(
     ),
     # Show a plot of the generated distribution
     mainPanel(
-      withSpinner(plotOutput("distPlot",height = '700px')),
-      p(),
-      verbatimTextOutput("pv"),
-     # actionButton("copyButton", "Copy!"),
-     h4("KEGG enrichment analysis"),
-     column(12,withSpinner(dataTableOutput('tablekegg'),proxy.height = '80px')),
-     h4("GeneOntology biological process enrichment analysis"),
-      column(12,withSpinner(dataTableOutput('tablego'),proxy.height = '80px')),
-     h4("Selected genes"),
-      column(12,dataTableOutput('table'))
+      tabsetPanel(
+        tabPanel("Start here", 
+                 h3('Welcome to VolcanoR web sevice'),
+                 h4("Short howto:"),
+                 h5("1. Prepare your differential expression results in tab separated format, you can see example",a(href = 'https://raw.githubusercontent.com/vovalive/volcanoR/master/res2_GSE70213.txt', 'HERE')),
+                 img(src="samplefile.png", height = 100),
+                 h5("2. Upload your file on left panel, and click 'Generate volcano plot' button"),
+                 h5("3. Now you can go to the Results tab and see generated plot"),
+                 h5("4. You can adjust tresholds and regenerate plot "),
+                 h5("5. When you like list of genes you selected, you can do KEGG and GO enrichment test by clicking button"),
+                 img(src="sampleplot.png", width = 300)
+                 ),
+        tabPanel("RESULTS",
+          withSpinner(plotOutput("distPlot",height = '700px')),
+          p(),
+          verbatimTextOutput("pv"),
+         # actionButton("copyButton", "Copy!"),
+         h4("KEGG enrichment analysis"),
+         column(12,withSpinner(dataTableOutput('tablekegg'),proxy.height = '80px')),
+         h4("GeneOntology biological process enrichment analysis"),
+          column(12,withSpinner(dataTableOutput('tablego'),proxy.height = '80px')),
+         h4("Selected genes"),
+          column(12,dataTableOutput('table')))
+      )
      )
 )))
 
@@ -145,8 +167,6 @@ server <- shinyServer(function(input, output) {
   output$tablekegg=renderDataTable(enrich()[[1]], options = list(pageLength = 5))
   output$tablego=renderDataTable(enrich()[[2]], options = list(pageLength = 5))
   })
-
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
